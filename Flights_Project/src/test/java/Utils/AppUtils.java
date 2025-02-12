cfc27a6f890edd8b9581de85ddfe70433ac971cd
LENOVO <LENOVO@DESKTOP-I5HKKLD>
2025-02-13 00:53:09 +0530

    AppUtils class, chrome driver and test suite added

--- Flights_Project/src/test/java/Utils/AppUtils.java
+++ Flights_Project/src/test/java/Utils/AppUtils.java
@@ -1,9 +1,31 @@
 package Utils;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
 public class AppUtils {
  
-	private void method1() {
+	public static WebDriver driver;
+	public static String url = "http://flights.qedgetech.com";
 	
-		System.out.println("This is TestCase1");
+	@BeforeSuite
+	public static void launchApp()
+	{	
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");		
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get(url);		
+	}
+	
+	@AfterSuite
+	public static void closeApp()
+	{
+		driver.close();
 	}
 }
